f1a84e138df4c29aeb9e08ccfa1f1a019b9cd2f8
drstonebraker <drew.stonebraker@gmail.com>
2017-10-28 21:07:01 -0700

    Write limited use object and createchant

--- private_vars.js
+++ private_vars.js
@@ -1,3 +1,7 @@
+// write a function that creates a bank account which holds private instance variables
+// such as account balance and return object with public methods for granting
+// authorized access to private vars
+
 const bankAccount = function bankAccount(initialBal, newPassword) {
   let balance = initialBal
   const passwordDigest = createPasswordDigest(newPassword)
@@ -12,3 +16,45 @@ const bankAccount = function bankAccount(initialBal, newPassword) {
     }
   }
 }
+
+
+// write a method that creates an object with a value that can only be accessed a
+// limited number of times
+
+const LimitedUseObject = function LimitUseObject(value) {
+  let remainingUses = 3
+
+  this.use = function () {
+    remainingUses--
+    return remainingUses >= 0 ? value : null
+  }
+}
+
+const myObj = new LimitedUseObject('abc');
+
+console.log(myObj.use())
+console.log(myObj.use())
+console.log(myObj.use())
+console.log(myObj.use())
+console.log(myObj.use())
+console.log(myObj.use())
+
+// write a function that uses a closure to add a new word to a chant on every call
+// of a function
+
+const createChant = function createChant() {
+
+  const chant = []
+
+  return function printChant(word) {
+    chant.push(word)
+    console.log(chant.join(' '))
+  }
+}
+
+const myChant = createChant()
+
+myChant('hey')
+myChant('hey')
+myChant('ho')
+myChant('ho')
