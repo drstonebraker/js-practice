03e014f5d863219435bcca59e121766831b065f5
drstonebraker <drew.stonebraker@gmail.com>
2017-10-29 14:45:21 -0700

    Rewrite function.prototype.curry

--- curry.js
+++ curry.js
@@ -7,28 +7,15 @@ function sumNums(...args) {
 // `curriedSum(1)(2)(3)`. After `numArgs` have been passed in, invoke the
 // original `fn` with the accumulated arguments, using `object` as the
 // context.
-function myCurry(fn, object, numArgs) {
-  const args = [];
 
-  const _curry = (newArg) => {
-    args.push(newArg);
-    if (args.length === numArgs) {
-      return fn.apply(object, args);
-    } else {
-      return _curry;
-    }
-  };
-
-  return _curry;
-}
-
-function curry(fn, object, numArgs) {
+function curry (fn, thisArg, numArgs) {
   const args = []
 
-  const _curry = function(arg) {
-    args.push(arg)
+  const _curry = function (newArg) {
+    args.push(newArg)
+
     if (args.length >= numArgs) {
-      return fn.apply(object, args)
+      return fn.apply(thisArg, args)
     } else {
       return _curry
     }
@@ -37,6 +24,21 @@ function curry(fn, object, numArgs) {
   return _curry
 }
 
+// function curry(fn, object, numArgs) {
+//   const args = []
+//
+//   const _curry = function(arg) {
+//     args.push(arg)
+//     if (args.length >= numArgs) {
+//       return fn.apply(object, args)
+//     } else {
+//       return _curry
+//     }
+//   }
+//
+//   return _curry
+// }
+//
 console.log(sumNums(1,2,3));
 const curriedSumNums = curry(sumNums, {}, 3)
 console.log(curriedSumNums(1));
@@ -46,8 +48,8 @@ console.log(curriedSumNums(3));
 Function.prototype.curry = function (context, numArgs) {
   const args = []
 
-  const _curry = (arg) => {
-    args.push(arg)
+  const _curry = (newArg) => {
+    args.push(newArg)
     if (args.length >= numArgs) {
       return this.apply(context, args)
     } else {
@@ -58,9 +60,24 @@ Function.prototype.curry = function (context, numArgs) {
   return _curry
 };
 
+// Function.prototype.curry = function (context, numArgs) {
+//   const args = []
+//
+//   const _curry = (arg) => {
+//     args.push(arg)
+//     if (args.length >= numArgs) {
+//       return this.apply(context, args)
+//     } else {
+//       return _curry
+//     }
+//   }
+//
+//   return _curry
+// };
+//
 console.log('#######');
 const arr = [1,2,3]
-console.log(arr.concat([4,5],[6,7]));
+console.log(arr.concat([4,5, 6],[7, 8, 9], [10,11,12]));
 const curriedConcat = arr.concat.curry(arr, 3)
 console.log(curriedConcat([4,5,6]));
 console.log(curriedConcat([7,8,9]));
